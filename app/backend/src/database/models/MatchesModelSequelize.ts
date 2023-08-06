fc538c860d43cb84de34fb10b42157ab5e254a46
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-08-06 16:28:51 -0300

    REQ 13

---
+++ app/backend/src/database/models/MatchesModelSequelize.ts
@@ -0,0 +1,58 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class MatchesModelSequelize extends Model<InferAttributes<MatchesModelSequelize>,
+InferCreationAttributes<MatchesModelSequelize>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: CreationOptional<number>;
+  declare homeTeamGoals: CreationOptional<number>;
+  declare awayTeamId: CreationOptional<number>;
+  declare awayTeamGoals: CreationOptional<number>;
+  declare inProgress: CreationOptional<boolean>;
+}
+
+MatchesModelSequelize.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_id',
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_id',
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    field: 'in_progress',
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+export default MatchesModelSequelize;
