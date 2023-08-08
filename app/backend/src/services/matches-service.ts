bceee35866d8c1e1c8a46d0ae5ad2d094b4fe343
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-08-07 21:07:59 -0300

    TFC FINALIZADO

--- app/backend/src/services/matches-service.ts
+++ app/backend/src/services/matches-service.ts
@@ -1,15 +1,6 @@
-/* eslint-disable sonarjs/cognitive-complexity */
-/* eslint-disable complexity */
-/* eslint-disable max-lines-per-function */
 import MatchesModel,
 { MatchInsertParams, MatchesGetByParams, MatchesUpdateParams } from '../models/MatchesModel';
 import { IMatches } from '../Interfaces';
-
-/**
- * Service da entidade Match
- * no constructor cria uma instância do model MatchesModel, que é um model abstrato
- * pra não misturar implementação lógica com abstrata
- */
 
 class MatchesService {
   private matchesModel: MatchesModel;
@@ -18,80 +9,91 @@ class MatchesService {
     this.matchesModel = new MatchesModel();
   }
 
+  private basePerformance = {
+    name: '',
+    totalPoints: 0,
+    totalGames: 0,
+    totalVictories: 0,
+    totalDraws: 0,
+    totalLosses: 0,
+    goalsFavor: 0,
+    goalsOwn: 0,
+  };
+
   getAll = async (): Promise<IMatches[] | null> => this.matchesModel.getAll();
+  getBy = async (params: MatchesGetByParams):
+  Promise<IMatches[] | null> => this.matchesModel.getBy(params);
 
-  getBy = async (params: MatchesGetByParams): Promise<IMatches[] | null> =>
-    this.matchesModel.getBy(params);
+  getById = async (id: number):
+  Promise<IMatches | null> => this.matchesModel.getById(id);
 
-  getById = async (id: number): Promise<IMatches | null> =>
-    this.matchesModel.getById(id);
+  update = async (params: MatchesUpdateParams):
+  Promise<IMatches | null> => this.matchesModel.update(params);
 
-  update = async (params: MatchesUpdateParams): Promise<IMatches | null> =>
-    this.matchesModel.update(params);
+  insert = async (params: MatchInsertParams):
+  Promise<IMatches | null> => this.matchesModel.insert(params);
 
-  insert = async (params: MatchInsertParams): Promise<IMatches | null> =>
-    this.matchesModel.insert(params);
+  private doPerformanceResponse = (params: DoPerformanceResponseParams): TeamPerformanceType => {
+    const { data: pData, isDraw, isHomeTeam, isLoss, isVictory, teamData } = params;
+    const totalVictories = pData.totalVictories + +(isHomeTeam ? isVictory : !isVictory);
+    const totalDraws = pData.totalDraws + +isDraw;
+    const totalLosses = pData.totalLosses + +(isHomeTeam ? isLoss : !isLoss);
+    let goalsFavor = pData.goalsFavor + (isHomeTeam
+      ? (teamData.homeTeamGoals - teamData.awayTeamGoals)
+      : (teamData.awayTeamGoals - teamData.homeTeamGoals));
+    goalsFavor = goalsFavor < 0 ? 0 : goalsFavor;
+    const goalsOwn = pData.goalsOwn + teamData.awayTeamGoals;
+    const name = (isHomeTeam ? teamData.homeTeam?.teamName || ''
+      : teamData.awayTeam?.teamName || '');
+    const totalGames = pData.totalGames + 1;
+    const totalPoints = (totalVictories * 3) + (totalDraws * 2);
+    return {
+      goalsFavor, goalsOwn, name, totalDraws, totalGames, totalLosses, totalPoints, totalVictories,
+    };
+  };
 
   getPerformance = async (isHomeTeam: boolean): Promise<TeamPerformanceType[]> => {
     const teamsData = await this.matchesModel.getAll() || [];
     const teamsPerformance: TeamPerformanceType[] = [];
+
     teamsData.forEach((teamData) => {
-      const teamPerformanceKey = teamsPerformance.findIndex(
-        (team) => team.name === teamData.homeTeam?.teamName,
-      );
-      const currentTeamPerformance = teamsPerformance[teamPerformanceKey];
-      const gameFinished = teamData.inProgress === false;
+      const currentTeamPerformance = teamsPerformance.find((team) => team.name
+        === (isHomeTeam ? teamData.homeTeam?.teamName : teamData.awayTeam?.teamName))
+        || this.basePerformance;
+      const isVictory = teamData.homeTeamGoals > teamData.awayTeamGoals;
+      const isDraw = teamData.homeTeamGoals === teamData.awayTeamGoals;
+      const isLoss = teamData.homeTeamGoals < teamData.awayTeamGoals;
+      const pData = this.doPerformanceResponse({
+        data: currentTeamPerformance, isDraw, isHomeTeam, isLoss, isVictory, teamData,
+      });
 
-      let isVictory = gameFinished && teamData.homeTeamGoals > teamData.awayTeamGoals;
-      isVictory = isHomeTeam ? isVictory : !isVictory;
-
-      const isDraw = gameFinished && teamData.homeTeamGoals === teamData.awayTeamGoals;
-
-      let isLoss = gameFinished && teamData.homeTeamGoals < teamData.awayTeamGoals;
-      isLoss = isHomeTeam ? isLoss : !isLoss;
-
-      let totalVictories = (currentTeamPerformance?.totalVictories || 0);
-      totalVictories = isVictory ? totalVictories + 1 : totalVictories;
-
-      let totalDraws = (currentTeamPerformance?.totalDraws || 0);
-      totalDraws = isDraw ? totalDraws + 1 : totalDraws;
-
-      let totalLosses = (currentTeamPerformance?.totalLosses || 0);
-      totalLosses = isLoss ? totalLosses + 1 : totalLosses;
-
-      const goalsFavor = (currentTeamPerformance?.goalsFavor || 0)
-      + teamData.homeTeamGoals + teamData.awayTeamGoals;
-
-      const goalsOwn = (currentTeamPerformance?.goalsOwn || 0) + teamData.awayTeamGoals;
-
-      const name = (isHomeTeam ? teamData.homeTeam?.teamName : teamData.awayTeam?.teamName) || '';
-
-      const data: TeamPerformanceType = {
-        name,
-        totalPoints: (totalVictories * 3) + (totalDraws * 2),
-        totalGames: (currentTeamPerformance?.totalGames || 0) + 1,
-        totalVictories,
-        totalDraws,
-        totalLosses,
-        goalsFavor,
-        goalsOwn,
-      };
-
-      if (teamPerformanceKey > -1) teamsPerformance[teamPerformanceKey] = data;
-      else teamsPerformance.push(data);
+      const teamPerformanceKey = teamsPerformance.findIndex((team) => team.name === pData.name);
+      if (teamPerformanceKey > -1) teamsPerformance[teamPerformanceKey] = pData;
+      else teamsPerformance.push(pData);
     });
+
     return teamsPerformance;
   };
 }
 
+type DoPerformanceResponseParams = {
+  data: TeamPerformanceType
+  teamData: IMatches
+  isVictory: boolean
+  isLoss: boolean
+  isDraw: boolean
+  isHomeTeam: boolean
+};
+
 type TeamPerformanceType = {
   name: string,
-  totalPoints: number
-  totalGames: number
+  totalPoints: number,
+  totalGames: number,
   totalVictories: number,
   totalDraws: number,
   totalLosses: number,
   goalsFavor: number,
   goalsOwn: number,
 };
+
 export default MatchesService;
